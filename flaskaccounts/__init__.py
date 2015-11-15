eeef655592c9dada9b6e78feac6852b56aca8c9d
JL <jia.long.loh@gmail.com>
2015-11-15 07:50:53 +0000

    Refactoring of existing user functions. Added conditional loop to login page to not show all other dropdown menus if not logged in

--- flaskaccounts/__init__.py
+++ flaskaccounts/__init__.py
@@ -1,8 +1,8 @@
 import sqlite3
 import os
-import hashlib
-from flask import Flask,request,session,g,redirect,url_for,render_template,flash
+from flask import Flask,request,session,g,redirect,url_for,render_template
 
+from .views.existinguser import existinguser
 
 accounts={'PREMIER':'PREMIER','ADVANCED':'ADVANCED','SAVINGS':'SAVINGS','TIME DEPOSITS':'TIME DEPOSITS','DSARA GW':'DSARA GROUNDWORKS','DSARA PL':'DSARA PILING','CASH':'CASH','DSARA IN':'DSARA IN'}
 currencylist=['GBP','MYR']
@@ -13,6 +13,8 @@ app.config.from_object('config') #looks for capitalized variables declared
 app.config.from_pyfile('config.py')
 
 app.config['DATABASE']=os.path.join(app.instance_path,'database.db')
+
+app.register_blueprint(existinguser)
 
 #session['logged_in']=False
 #session.pop('username',None)
@@ -37,47 +39,6 @@ def add_header(response):
 	return response
 
 #view functions
-@app.route('/login',methods=['GET','POST'])
-def login():
-	error=None
-	if request.method=='POST':
-		username=request.form['username']
-		password=request.form['password']
-
-		userdict={}
-		for row in g.db.execute('SELECT username,salt,hash from users'):
-			usernames,salt,hash1=row
-			userdict[usernames]=[salt,hash1]
-
-
-		if username not in userdict.keys():
-			error='Unregistered User'
-		#elif password != (app.config['USERS'])[username]:
-		#	error='Invalid Password'
-		else:
-			#unencode the salt
-			usersalt=userdict[username][0].decode('base64')
-			#usersalted=usersalt+str(password)
-			hashresult=hashlib.sha256(userdict[username][0]+str(password)).hexdigest()
-			#hashed_object=hashlib.sha256(usersalt+str(password))
-			#compare hashes
-			if hashresult!=userdict[username][1]:
-				error='Password Does Not Match'
-			else:
-				session['logged_in']=True
-				session['username']=username
-				flash('Log In Successful')
-				return redirect(url_for('listall',no_rows_limit=20))
-
-	return render_template('login.html',error=error)
-
-@app.route('/logout',methods=['GET','POST'])
-def logout():
-	session['logged_in']=False
-	session.pop('username',None)
-	return redirect(url_for('login'))
-	#return render_template('')
-
 
 @app.route('/show',defaults={'no_rows_limit':None})
 @app.route('/show/<int:no_rows_limit>')
---
+++ flaskaccounts/views/existinguser.py
@@ -0,0 +1,45 @@
+from flask import Blueprint,render_template,g,request,redirect,url_for,session,flash
+import hashlib
+
+existinguser=Blueprint('existinguser',__name__)
+
+@existinguser.route('/login',methods=['GET','POST'])
+def login():
+	error=None
+	if request.method=='POST':
+		username=request.form['username']
+		password=request.form['password']
+
+		userdict={}
+		for row in g.db.execute('SELECT username,salt,hash from users'):
+			usernames,salt,hash1=row
+			userdict[usernames]=[salt,hash1]
+
+
+		if username not in userdict.keys():
+			error='Unregistered User'
+		#elif password != (app.config['USERS'])[username]:
+		#	error='Invalid Password'
+		else:
+			#unencode the salt
+			usersalt=userdict[username][0].decode('base64')
+			#usersalted=usersalt+str(password)
+			hashresult=hashlib.sha256(userdict[username][0]+str(password)).hexdigest()
+			#hashed_object=hashlib.sha256(usersalt+str(password))
+			#compare hashes
+			if hashresult!=userdict[username][1]:
+				error='Password Does Not Match'
+			else:
+				session['logged_in']=True
+				session['username']=username
+				flash('Log In Successful')
+				return redirect(url_for('listall',no_rows_limit=20))
+
+	return render_template('users/login.html',error=error)
+
+@existinguser.route('/logout',methods=['GET','POST'])
+def logout():
+	session['logged_in']=False
+	session.pop('username',None)
+	return redirect(url_for('.login'))
+	#return render_template('')
